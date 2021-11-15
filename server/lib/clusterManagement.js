0102841e3edc6849daba09acebe0ec065c9ea8d8
Nicholas Anna <nickanna_42@yahoo.com>
2021-11-14 21:19:08 -0500

    Hook server into database

--- server/lib/clusterManagement.js
+++ server/lib/clusterManagement.js
@@ -1,6 +1,8 @@
 // This file contains the operations that need to
 // be performed before the cluster can fork and the
 // HttpServer threads can be instantiated.
+const CONFIG = require('../config');
+const { Pool } = require('pg');
 
 function masterStart() {
   return new Promise(function(resolve, reject) {
@@ -10,6 +12,12 @@ function masterStart() {
 
 function forkStart() {
   return new Promise(function(resolve, reject) {
+    global.dbPool = new Pool({
+      user: CONFIG.DB_USER,
+      password: CONFIG.DB_PASS,
+      host: CONFIG.DB_HOST,
+      port: CONFIG.DB_PORT,
+    });
     resolve();
   });
 }
